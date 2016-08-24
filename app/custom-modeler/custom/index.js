6f8a23f2ccdab2173a8ed59d64ca676b4d40ee42
Philipp Fromme <philipp.fromme@camunda.com>
2016-08-24 15:09:04 +0200

    Remove customizations

--- app/custom-modeler/custom/index.js
+++ app/custom-modeler/custom/index.js
@@ -1,9 +1,3 @@
 module.exports = {
-  __init__: [ 'customRenderer', 'paletteProvider', 'customRules', 'customUpdater', 'contextPadProvider' ],
-  elementFactory: [ 'type', require('./CustomElementFactory') ],
-  customRenderer: [ 'type', require('./CustomRenderer') ],
-  paletteProvider: [ 'type', require('./CustomPalette') ],
-  customRules: [ 'type', require('./CustomRules') ],
-  customUpdater: [ 'type', require('./CustomUpdater') ],
-  contextPadProvider: [ 'type', require('./CustomContextPadProvider') ]
+  // our modules go here
 };
