a0bd77ffc31ff563bf086f9123ab81745e5f7b9c
pedesen <patrick.dehn@camunda.com>
2016-08-24 15:45:19 +0200

    add properties panel infrastructure

--- app/app.js
+++ app/app.js
@@ -1,10 +1,27 @@
 'use strict';
 
+var propertiesPanelModule = require('bpmn-js-properties-panel'),
+    propertiesProviderModule = require('./custom-properties-panel/providers/music'),
+    musicModdleDescriptor = require('./custom-properties-panel/descriptors/music');
+
 var CustomModeler = require('./custom-modeler');
 
 var modeler = new CustomModeler({
   container: '#canvas',
-  keyboard: { bindTo: document }
+  keyboard: { bindTo: document },
+  propertiesPanel: {
+    parent: '#js-properties-panel'
+  },
+  additionalModules: [
+    propertiesPanelModule,
+    propertiesProviderModule
+  ],
+  moddleExtensions: {
+    music: musicModdleDescriptor
+  }
 });
+
+
+modeler.createDiagram();
 
 window.bpmnjs = modeler;
